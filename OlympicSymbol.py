9ccf0ed58056a896ad9933262ad8b23246d88524
getwildysy <42105084+getwildysy@users.noreply.github.com>
2018-08-07 23:40:13 +0800

    123

--- OlympicSymbol.py
+++ OlympicSymbol.py
@@ -1,9 +1,9 @@
 import turtle
 
-turtle.color("blue")
-turtle.penup()
-turtle.goto(-110, -25)
-turtle.pendown()
+turtle.color("blue")  #顏色藍色
+turtle.penup()         #舉起筆
+turtle.goto(-110, -25)  
+turtle.pendown()       #放下筆
 turtle.circle(45)
 
 turtle.color("black")
@@ -30,4 +30,4 @@ turtle.goto(55, -75)
 turtle.pendown()
 turtle.circle(45)
 
-turtle.done() 
+turtle.done()  #結束
